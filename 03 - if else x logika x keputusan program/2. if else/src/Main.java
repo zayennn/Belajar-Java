8f46b5274bbeafcea01fabece4f0003e5024cf2c
zayennn <zaayeennn@gmail.com>
2026-02-21 21:58:55 +0700

    update

--- 03 - if else x logika x keputusan program/2. if else/src/Main.java
+++ 03 - if else x logika x keputusan program/2. if else/src/Main.java
@@ -4,13 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("masukan umur kamu");
+        System.out.print("masukan umur kamu : ");
         int umur = input.nextInt();
 
-        if ( umur >= 19 ) {
+        if (umur >= 19) {
             System.out.print("Dewasa");
         } else {
             System.out.print("Remaja");
         }
     }
 }
+
+//  if (kondisi) {
+//     aksi true;
+//  } else {
+//      aksi false;
+//  }
