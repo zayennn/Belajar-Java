a3836f7b04379a2f448eca9eb1f497a1f1f7ba6d
zayennn <zaayeennn@gmail.com>
2026-02-21 21:29:54 +0700

    update

--- 02 - Input User Operator Perhitungan/2. Operator Matematika/src/Main.java
+++ 02 - Input User Operator Perhitungan/2. Operator Matematika/src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void tambah(int x, int y) {
+        System.out.printf("hasil dari %d + %d adalah = %d\n", x, y, x + y);
+    }
+    public static void kali(int x, int y) {
+        System.out.printf("hasil dari %d × %d adalah = %d\n", x, y, x * y);
+    }
+    public static void bagi(int x, int y) {
+        System.out.printf("hasil dari %d : %d adalah = %d\n", x, y, x / y);
+    }
+    public static void kurang(int x, int y) {
+        System.out.printf("hasil dari %d - %d adalah = %d\n", x, y, x - y);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Argumen ke-0: " + args[0]);
-        System.out.println("Argumen ke-1: " + args[1]);
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("masukan angka pertama : ");
+        int x = input.nextInt();
+
+        System.out.print("masukan angka kedua : ");
+        int y = input.nextInt();
+
+        tambah(x, y);
+        kali(x, y);
+        bagi(x, y);
+        kurang(x, y);
+
+        input.close();
     }
 }
---
+++ 02 - Input User Operator Perhitungan/3. Operator Perbandingan/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int umur = 19;
+
+        System.out.println(umur >= 19);
+    }
+}
+
+//| Operator | Arti             |
+//        | -------- | ---------------- |
+//        | ==       | sama dengan      |
+//        | !=       | tidak sama       |
+//        | >        | lebih besar      |
+//        | <        | lebih kecil      |
+//        | >=       | lebih besar sama |
+//        | <=       | lebih kecil sama |
