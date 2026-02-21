7a8ca535d3c93ec9a049cf026ad0cdd08f5b2546
zayennn <zaayeennn@gmail.com>
2026-02-21 21:53:31 +0700

    update

---
+++ 03 - if else x logika x keputusan program/1. if/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int umur = 19;
+
+        if ( umur >= 19 ) {
+            System.out.print("dewasa");
+        }
+    }
+}
