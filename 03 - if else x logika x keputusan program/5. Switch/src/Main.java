501814bdb2c805cf678aa58d9c23b77f04b6c2a5
zayennn <zaayeennn@gmail.com>
2026-02-21 22:09:40 +0700

    update

---
+++ 03 - if else x logika x keputusan program/5. Switch/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int menu = 3;
+
+        switch (menu) {
+            case 1:
+                System.out.println("mie ayam");
+                break;
+            case 2:
+                System.out.println("kwetiaw");
+                break;
+            case 3:
+                System.out.println("bakso");
+                break;
+            case 4:
+                System.out.println("bakmi");
+                break;
+            case 5:
+                System.out.println("rendang");
+                break;
+        }
+    }
+}
