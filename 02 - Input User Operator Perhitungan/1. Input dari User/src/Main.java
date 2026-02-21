69bbb4475d96c42d3e5539f8afa4976a8aadb39f
zayennn <zaayeennn@gmail.com>
2026-02-21 20:34:02 +0700

    update

---
+++ 02 - Input User Operator Perhitungan/1. Input dari User/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("masukan nama kamu: ");
+        String nama = input.nextLine();
+
+        System.out.println("halo " + nama);
+    }
+}
+
