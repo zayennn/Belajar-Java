cb04c2b2c89d874c116f8bc7006e955cbc10b722
zayennn <zaayeennn@gmail.com>
2026-02-21 19:19:48 +0700

    update

--- 01 — Fundamental Java/2. Variabel/src/Main.java
+++ 01 — Fundamental Java/2. Variabel/src/Main.java
@@ -2,13 +2,15 @@ public class Main {
     public static void main(String[] args) {
         String nama = "elang";
         int umur = 19;
+        String kota = "bogor";
         double tinggi = 175.5;
-        boolean ganteng = true;
+        boolean mahasiswa = true;
 
         System.out.println(nama);
         System.out.println(umur);
+        System.out.println(kota);
         System.out.println(tinggi);
-        System.out.println(ganteng);
+        System.out.println(mahasiswa);
     }
 }
 
@@ -18,3 +20,10 @@ public class Main {
 //  | int     | bilangan bulat | 20      |
 //  | double  | angka desimal  | 170.5   |
 //  | boolean | true / false   | true    |
+
+
+// String nama = "elang"; : bikin variable teks bernama 'nama'
+// int umur = 19; : angka tanpa koma
+// double tinggi = 175.5; : angka pakai koma
+// boolean ganteng = true; : nilai logika
+//
