92050d819630306cdc43c20c679b65b5b298f94e
zayennn <zaayeennn@gmail.com>
2026-02-21 22:59:08 +0700

    update

--- 03 - if else x logika x keputusan program/4. Operator logika/src/Main.java
+++ 03 - if else x logika x keputusan program/4. Operator logika/src/Main.java
@@ -5,8 +5,22 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.print("masukan total jumlah belanjaan : Rp ");
-        int total_belanja = input.nextInt();
+        double total_belanja = input.nextDouble();
+        double diskon = 0;
 
+        if (total_belanja >= 500000) {
+            diskon = total_belanja * 0.2;
+        } else if (total_belanja >= 200000) {
+            diskon = total_belanja * 0.1;
+        } else {
+            diskon = 0;
+        }
+
+        System.out.printf("total belanja = Rp %.2f\n", total_belanja);
+        System.out.printf("diskon = Rp %.2f -\n", diskon);
+        System.out.printf("total bayar = Rp %.2f\n", total_belanja - diskon);
+
+        input.close();
     }
 }
 
