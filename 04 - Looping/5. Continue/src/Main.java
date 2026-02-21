e6f9f5396c9a007cd8162533b64dddd21566c801
zayennn <zaayeennn@gmail.com>
2026-02-21 23:31:32 +0700

    update

---
+++ 04 - Looping/5. Continue/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 3) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
+
+// angka 3 di skip
