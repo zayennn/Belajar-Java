0328bd57a922bf9715a662efd9edd40bd5b839b8
zayennn <zaayeennn@gmail.com>
2026-02-21 22:02:59 +0700

    update

--- 03 - if else x logika x keputusan program/4. Operator logika/src/Main.java
+++ 03 - if else x logika x keputusan program/4. Operator logika/src/Main.java
@@ -1,1 +1,16 @@
-public
+public class Main {
+    public static void main(String[] args) {
+        int umur = 20;
+        boolean punyaKTP = true;
+
+        if (umur >= 17 && punyaKTP) {
+            System.out.println("Boleh bikin SIM");
+        }
+    }
+}
+
+//  | Operator | Arti      |
+//  | -------- | --------- |
+//  | &&       | dan       |
+//  | ||       | atau      |
+//  | !        | kebalikan |
