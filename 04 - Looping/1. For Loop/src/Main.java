17e9418dddece8fed0bb24438ee5d37d819263dd
zayennn <zaayeennn@gmail.com>
2026-02-21 23:11:26 +0700

    update

---
+++ 04 - Looping/1. For Loop/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+//  for (inisialisasi; kondisi; perubahan) {
+//      aksi;
+//  }
