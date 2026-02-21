848c3cab7f03917baa6101e3261d63c18e7a585c
zayennn <zaayeennn@gmail.com>
2026-02-21 23:26:36 +0700

    update

--- 04 - Looping/3. Do While/src/Main.java
+++ 04 - Looping/3. Do While/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int i = 1;
 
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
     }
 }
+
+// beda nya dengan while : kondisi dicek setelah eksekusi.
+// Walaupun kondisi false → tetap jalan sekali.
