0b3e49b4a0154fd37b8fa15f272e4c03ff203583
zayennn <zaayeennn@gmail.com>
2026-02-21 22:01:30 +0700

    update

---
+++ 03 - if else x logika x keputusan program/4. if else if/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int nilai = 85;
+
+        if (nilai >= 90) {
+            System.out.println("A");
+        } else if (nilai >= 80) {
+            System.out.println("B");
+        } else if (nilai >= 70) {
+            System.out.println("C");
+        } else {
+            System.out.println("D");
+        }
+    }
+}
+
+//   if (kondisi1) {
+//   }
+//   else if (kondisi2) {
+//   }
+//   else {
+//   }
