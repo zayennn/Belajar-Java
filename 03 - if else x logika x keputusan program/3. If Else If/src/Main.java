f2c6758e93d277fa88b4f1c4d4d2b16bde00a4bc
zayennn <zaayeennn@gmail.com>
2026-02-21 22:42:19 +0700

    update

--- 03 - if else x logika x keputusan program/3. If Else If/src/Main.java
+++ 03 - if else x logika x keputusan program/3. If Else If/src/Main.java
@@ -4,8 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("masukan nilai kamu");
+        System.out.print("masukan nilai kamu : ");
         int nilai = input.nextInt();
+
+        if (nilai < 0 || nilai >= 100) {
+            System.out.println("nilai tidak valid, nilai hanya boleh 0 - 100");
+        } else if (nilai > 90) {
+            System.out.println("A");
+        } else if (nilai > 80) {
+            System.out.println("B");
+        } else if (nilai > 70) {
+            System.out.println("C");
+        } else  {
+            System.out.println("D");
+        }
     }
 }
 
