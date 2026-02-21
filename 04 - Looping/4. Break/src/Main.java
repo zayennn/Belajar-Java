6bc1776146f8cab87dd80b531b3fb2e52167ba0e
zayennn <zaayeennn@gmail.com>
2026-02-21 23:29:46 +0700

    update

---
+++ 04 - Looping/4. Break/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
+
+// output cuma sampai 4 ( karena index dimulai dari 0 )
