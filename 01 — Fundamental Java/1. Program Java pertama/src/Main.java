6e215217de240faead64e86d827d1743a5c396fd
zayennn <zaayeennn@gmail.com>
2026-02-21 19:12:32 +0700

    upload project

---
+++ 01 — Fundamental Java/1. Program Java pertama/src/Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("halo nama gua elang");
+        System.out.println("gua lagi belajar java");
+    }
+}
+
+// public class Main :
+// class = wadah program
+// Main = nama class ( harus sama dengan nama file, contoh: Main.java )
+// public = bisa di akses dimana aja
+// class itu ibarat rumah , semua code di dalem nya itu ada di dalem nya
+
+
+// public static void main : ini pintu masuk program, java mulai eksekusi itu dari sini
+// public = bisa diakases dari mana aja
+// static = gaperlu bikin object
+// void = ga ngembaliin nilai
+// main = nama method utama
+// String[] args = parameter dari command line
+
+// System.out.println : ini buat nampilin teks ke layar/terminal
+// System = sistem java
+// out = output stream
+// println = print + bikin baris baru
---
+++ 01 — Fundamental Java/2. Variabel/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String nama = "elang";
+        int umur = 19;
+        double tinggi = 175.5;
+        boolean ganteng = true;
+
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(tinggi);
+        System.out.println(ganteng);
+    }
+}
+
+//  | Tipe    | Isi            | Contoh  |
+//  | ------- | -------------- | ------- |
+//  | String  | teks           | "Elang" |
+//  | int     | bilangan bulat | 20      |
+//  | double  | angka desimal  | 170.5   |
+//  | boolean | true / false   | true    |
