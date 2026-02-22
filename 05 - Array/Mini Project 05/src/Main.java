60af01726c3efe7c7950f1d640f49c9bf158ff2f
zayennn <zaayeennn@gmail.com>
2026-02-23 00:00:53 +0700

    update

--- 05 - Array/Mini Project 05/src/Main.java
+++ 05 - Array/Mini Project 05/src/Main.java
@@ -4,13 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int max = 0;
-        int min = 0;
-
         System.out.print("masukan jumlah mahasiswa : ");
         int jumlah_siswa = input.nextInt();
 
         int[] nilai = new int[jumlah_siswa];
+        int max = 0;
+        int min = 100;
 
         for (int i = 0; i < jumlah_siswa; i++) {
             System.out.println("masukan nilai mahasiswa ke - " + (i + 1) + " : ");
@@ -19,6 +18,16 @@ public class Main {
 
         for (int i = 0; i < jumlah_siswa; i++) {
             System.out.printf("mahasiswa ke - %d mempunyai nilai = %d\n", i + 1, nilai[i]);
+
+            if (nilai[i] > max) {
+                max = nilai[i];
+            }
+            if (nilai[i] < min) {
+                min = nilai[i];
+            }
         }
+
+        System.out.println("\nnilai terbesar : " + max);
+        System.out.println("nilai terkecil : " + min);
     }
 }
