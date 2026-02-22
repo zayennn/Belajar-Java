b4966fbf0ad8d7d9def50390fd7a4b953ecc307c
zayennn <zaayeennn@gmail.com>
2026-02-22 23:31:15 +0700

    update

--- 05 - Array/Latihan 05/src/Main.java
+++ 05 - Array/Latihan 05/src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 //        Latihan 1 — tampilkan semua isi array
         int[] arr = {5, 10, 15, 20, 25};
 
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.println(arr[i]);
-//        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
 //        Latihan 2 — jumlah semua isi array
         int total = 0;
