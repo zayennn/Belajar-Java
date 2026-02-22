5b2ef038e243f0d24b60c7295c09a089b1b919d5
zayennn <zaayeennn@gmail.com>
2026-02-22 23:41:59 +0700

    update

--- 05 - Array/Mini Project 05/src/Main.java
+++ 05 - Array/Mini Project 05/src/Main.java
@@ -6,6 +6,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("masukan jumlah mahasiswa : ");
+        int jumlah_siswa = input.nextInt();
+
+        int[] nilai = new int[jumlah_siswa];
+
+        for (int i = 0; i < jumlah_siswa; i++) {
+            System.out.println("");
+        }
+
+        System.out.println(nilai[0]);
     }
 }
