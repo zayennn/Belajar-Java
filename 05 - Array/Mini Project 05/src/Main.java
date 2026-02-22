815e49944299a892324b38165c15f65904b511ab
zayennn <zaayeennn@gmail.com>
2026-02-22 23:52:46 +0700

    update

--- 05 - Array/Mini Project 05/src/Main.java
+++ 05 - Array/Mini Project 05/src/Main.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void mahasiswa(int mhs, int nilai) {
-
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int max = 0;
+        int min = 0;
 
         System.out.print("masukan jumlah mahasiswa : ");
         int jumlah_siswa = input.nextInt();
@@ -14,9 +13,12 @@ public class Main {
         int[] nilai = new int[jumlah_siswa];
 
         for (int i = 0; i < jumlah_siswa; i++) {
-            System.out.println("");
+            System.out.println("masukan nilai mahasiswa ke - " + (i + 1) + " : ");
+            nilai[i] = input.nextInt();
         }
 
-        System.out.println(nilai[0]);
+        for (int i = 0; i < jumlah_siswa; i++) {
+            System.out.printf("mahasiswa ke - %d mempunyai nilai = %d\n", i + 1, nilai[i]);
+        }
     }
 }
