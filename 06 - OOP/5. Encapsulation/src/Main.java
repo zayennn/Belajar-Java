d1e223c6f8f20f9de0f6f16cd7f9a1d60cea5a0c
zayennn <zaayeennn@gmail.com>
2026-02-26 04:50:24 +0700

    update

--- 06 - OOP/5. Encapsulation/src/Main.java
+++ 06 - OOP/5. Encapsulation/src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main{
+    static class Mahasiswa{
+        private String nama;
+        private int umur;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Mahasiswa(String nama, int umur) {
+            this.nama = nama;
+            this.umur = umur;
         }
+
+        public String tampilkanNama() {
+            return nama;
+        }
+
+        public int tampilkanUmur() {
+            return umur;
+        }
+    }
+
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa("capytanic", 12);
+        System.out.printf("halo %s, umur kamu %d tahun", mhs1.tampilkanNama(), mhs1.tampilkanUmur());
     }
 }
