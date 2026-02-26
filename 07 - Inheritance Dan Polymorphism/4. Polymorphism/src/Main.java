aa0b28954de4141db78ce99bb47e6e9b3bd60729
zayennn <zaayeennn@gmail.com>
2026-02-27 06:00:01 +0700

    update

--- 07 - Inheritance Dan Polymorphism/4. Polymorphism/src/Main.java
+++ 07 - Inheritance Dan Polymorphism/4. Polymorphism/src/Main.java
@@ -1,1 +1,30 @@
-public class Main{}
+public class Main{
+    static class Kendaraan{
+        void suara(){
+            System.out.println("suara umum");
+        }
+    }
+
+    static class Mobil extends Kendaraan {
+        void suara(){
+            System.out.println("bruum");
+        }
+    }
+
+    static class Motor extends Kendaraan {
+        void suara(){
+            System.out.println("ngengggg");
+        }
+    }
+
+    public static void main(String[] args) {
+        Kendaraan[] daftar = {
+                new Mobil();
+                new Motor();
+        }
+
+        for (Kendaraan k : daftar) {
+            k.suara();
+        }
+    }
+}
