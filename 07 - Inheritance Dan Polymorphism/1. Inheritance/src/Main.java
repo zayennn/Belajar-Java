7a5f3be4c564e1cb52a9b0ff8b3d918bdde594ca
zayennn <zaayeennn@gmail.com>
2026-02-26 22:30:39 +0700

    update

--- 07 - Inheritance Dan Polymorphism/1. Inheritance/src/Main.java
+++ 07 - Inheritance Dan Polymorphism/1. Inheritance/src/Main.java
@@ -1,13 +1,24 @@
-public class Main{
-    class Kendaraan{
-        String merk;
+public class Main {
 
-        void info(){
+    // Ini class dasar / induk.
+    // Semua kendaraan minimal punya merk.
+    // Jadi kita taro di parent biar gak nulis ulang di class lain.
+    class Kendaraan {
+
+        String merk; // properti dasar yang pasti dimiliki kendaraan
+
+        void info() {
+            // method ini buat nampilin info dasar kendaraan
             System.out.println("mobil merek : " + merk);
         }
     }
 
-    class Mobil extends Kendaraan{
+    // Mobil itu turunan dari Kendaraan
+    // Artinya Mobil otomatis punya semua isi Kendaraan (merk + method info())
+    class Mobil extends Kendaraan {
+
         int jumlahPintu;
+        // properti tambahan yang cuma dimiliki Mobil
+        // Kendaraan umum gak harus punya pintu
     }
 }
