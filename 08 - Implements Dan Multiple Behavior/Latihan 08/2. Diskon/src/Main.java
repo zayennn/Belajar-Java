46075f88217087a00b153caa5a7813e4d49237d8
zayennn <zaayeennn@gmail.com>
2026-02-28 09:24:35 +0700

    update

--- 08 - Implements Dan Multiple Behavior/Latihan 08/2. Diskon/src/Main.java
+++ 08 - Implements Dan Multiple Behavior/Latihan 08/2. Diskon/src/Main.java
@@ -1,3 +1,28 @@
+interface Diskon{
+    double getHitungDiskon(double harga);
+}
+
+class MemberGold implements Diskon{
+    public double getHitungDiskon(double harga) {
+        return harga * 0.2;
+    }
+}
+
+class MemberSilver implements Diskon{
+    public double getHitungDiskon(double harga) {
+        return harga * 0.1;
+    }
+}
+
 public class Main{
-    public
+    public static void main(String[] args) {
+        MemberGold person1 = new MemberGold();
+        person1.getHitungDiskon(100000);
+
+        MemberSilver person2 = new MemberSilver();
+        person2.getHitungDiskon(100000);
+
+        System.out.println(person1);
+        System.out.println(person2);
+    }
 }
