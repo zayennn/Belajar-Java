0e7b385726cbf4dd861fe4a853d970c815bd9135
zayennn <zaayeennn@gmail.com>
2026-03-01 09:21:14 +0700

    update

--- 08 - Implements Dan Multiple Behavior/Latihan 08/3. Inheritance x Interface/src/Main.java
+++ 08 - Implements Dan Multiple Behavior/Latihan 08/3. Inheritance x Interface/src/Main.java
@@ -1,5 +1,32 @@
-interface Electric{
-    void getElectricInfo();
+class Vehicle{
+    String merk;
+
+    Vehicle(String merk) {
+        this.merk = merk;
+    }
+
+    void start(){
+        System.out.println("jalan");
+    }
+
+    void stop() {
+        System.out.println("berhenti");
+    }
+}
+
+interface Electric {
+    void isiDaya();
+    double getCekBatre();
+}
+
+class Tesla extends Vehicle implements Electric {
+    int batre_sekarang;
+
+    Tesla(String merk, int batre_sekarang) {
+        super(merk);
+    }
+
+
 }
 
 public class Main{
