fb81ae3c0546d7cbe49a77951de4912324ff1960
zayennn <zaayeennn@gmail.com>
2026-03-05 01:18:56 +0700

    update

---
+++ 09 - Generics Dan Collections/1. Generics/src/Main.java
@@ -0,0 +1,11 @@
+// kalo sebelum nya pake : Hewan[] daftar = new Hewan[3]; → ini nilai nya fix, kaku, primitif
+/*
+* di dunia nyata pakai :
+*   • ArrayList
+*   • HashMap
+*   • Set
+*   • Queue
+*   • Generics
+*   • dll
+*/
+
