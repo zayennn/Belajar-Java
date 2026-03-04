36f547064864636556f979719673bb0775a40fed
zayennn <zaayeennn@gmail.com>
2026-03-05 01:31:31 +0700

    update

---
+++ 09 - Generics Dan Collections/3. HashMap - Key Value/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> products = new HashMap<>();
+
+        products.put("BMW", 1000000000);
+        products.put("H2", 800000000);
+
+        System.out.println(products.get("BMW"));
+    }
+}
