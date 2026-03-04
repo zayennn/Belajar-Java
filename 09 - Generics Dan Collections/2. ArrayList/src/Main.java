2aada102e1745f17953afd6267c6f32418012979
zayennn <zaayeennn@gmail.com>
2026-03-05 01:25:21 +0700

    update

---
+++ 09 - Generics Dan Collections/2. ArrayList/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> daftar = new ArrayList<>();
+
+        daftar.add("H2");
+        daftar.add("BMW");
+
+        System.out.println(daftar.get(0));
+    }
+}
