80ede36c6b5674f62eda0750617d82843e02b5c4
zayennn <zaayeennn@gmail.com>
2026-03-07 02:30:28 +0700

    update

---
+++ 09 - Generics Dan Collections/Latihan 09/4. Sistem Leaderboard Game/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main{
+    static class Player {
+        private String nama;
+        private int score;
+
+        Player(String nama, int score) {
+            this.nama = nama;
+            this.score = score;
+        }
+
+        void getInfo(int count) {
+            System.out.printf("""
+========== Player Ke - %d ==========
+Nama    :   %s
+Score   :   %d
+
+                    """, count, nama, score);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Player> players = new ArrayList<>();
+        int total = 0;
+        int max = 0;
+        int rata_rata;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan jumlah player : ");
+        int jum_players = input.nextInt();
+        input.nextLine();
+
+        for (int i = 0; i < jum_players; i++) {
+            System.out.printf("Masukan nama player ke - %d : ", i + 1);
+            String nama = input.nextLine();
+
+            System.out.printf("Masukan score player ke - %d : ", i + 1);
+            int score = input.nextInt();
+            input.nextLine();
+
+            Player p = new Player(nama, score);
+            players.add(p);
+        }
+
+        int count = 1;
+        for (Player p : players) {
+            if (max < p.score) {
+                max = p.score;
+            }
+
+            p.getInfo(count);
+            count++;
+
+            System.out.println("Score tertinggi adalah : " + max);
+        }
+    }
+}
