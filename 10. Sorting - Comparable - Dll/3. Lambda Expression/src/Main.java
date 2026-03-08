3c3af7f15c1d1daaec2857fc81449ba91ef426ff
zayennn <zaayeennn@gmail.com>
2026-03-09 01:19:45 +0700

    update

--- 10. Sorting - Comparable - Dll/3. Lambda Expression/src/Main.java
+++ 10. Sorting - Comparable - Dll/3. Lambda Expression/src/Main.java
@@ -5,3 +5,25 @@
 
     contoh :
 */
+
+import java.util.ArrayList;
+
+public class Main {
+    static class Player {
+        String nama;
+        int score;
+
+        Player(String nama, int score) {
+            this.nama = nama;
+            this.score = score;
+        }
+
+        void getInfo() {
+            System.out.printf("""
+                    nama    : %s
+                    score   : %d
+                    
+                    """, nama, score);
+        }
+    }
+}
