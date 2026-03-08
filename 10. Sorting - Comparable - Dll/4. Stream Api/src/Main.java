304b27492b96d367e175652dc61e8faf0822854b
zayennn <zaayeennn@gmail.com>
2026-03-09 01:29:45 +0700

    update

--- 10. Sorting - Comparable - Dll/4. Stream Api/src/Main.java
+++ 10. Sorting - Comparable - Dll/4. Stream Api/src/Main.java
@@ -1,3 +1,13 @@
 // ini adalah java modern backend
 // Dipakai heavily di Spring Boot.
 // konsepnya : data → stream → filter/map/sort → output
+// contoh program :
+
+import java.util.ArrayList;
+
+public class Main {
+    static class Player {
+        String nama;
+        int score;
+    }
+}
