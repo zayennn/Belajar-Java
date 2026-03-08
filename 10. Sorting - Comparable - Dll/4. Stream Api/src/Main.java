b5ddb6d651eb180809e02d27a671fe9eb8b07519
zayennn <zaayeennn@gmail.com>
2026-03-09 01:35:57 +0700

    update

--- 10. Sorting - Comparable - Dll/4. Stream Api/src/Main.java
+++ 10. Sorting - Comparable - Dll/4. Stream Api/src/Main.java
@@ -29,6 +29,9 @@ public class Main {
 
         players.addAll(Arrays.asList(
                 new Player("capytanic", 370),
+                new Player("capyking", 390),
+                new Player("capytanic", 370),
+                new Player("capytanic", 370),
         ));
     }
 }
