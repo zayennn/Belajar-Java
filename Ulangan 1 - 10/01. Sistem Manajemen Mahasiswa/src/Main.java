9b6c4f36348c01820c89e6eeaf2b200c13d7724c
zayennn <zaayeennn@gmail.com>
2026-03-10 06:04:36 +0700

    update

--- Ulangan 1 - 10/01. Sistem Manajemen Mahasiswa/src/Main.java
+++ Ulangan 1 - 10/01. Sistem Manajemen Mahasiswa/src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static class Mahasiswa {
+        String nama;
+        int nim;
+        int nilai;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Mahasiswa(String nama, int nim, int nilai) {
+            this.nama = nama;
+            this.nim = nim;
+            this.nilai = nilai;
+        }
+
+        void getInfo() {
+
         }
     }
 }
