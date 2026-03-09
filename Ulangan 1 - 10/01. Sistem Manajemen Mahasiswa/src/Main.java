159e687af98713bccc9f1bb0ee7d120b3614a7cb
zayennn <zaayeennn@gmail.com>
2026-03-10 06:09:21 +0700

    update

--- Ulangan 1 - 10/01. Sistem Manajemen Mahasiswa/src/Main.java
+++ Ulangan 1 - 10/01. Sistem Manajemen Mahasiswa/src/Main.java
@@ -26,6 +26,10 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Mahasiswa> mahasiswas = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan jumlah mahasiswa : ");
+        int jumlah_mahasiswa = input.nextInt();
 
     }
 }
