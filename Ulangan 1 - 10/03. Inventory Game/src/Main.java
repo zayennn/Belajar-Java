a26d6b1e7dcbda580b15ddcfbfc8015f40801f59
zayennn <zaayeennn@gmail.com>
2026-03-10 07:21:45 +0700

    update

--- Ulangan 1 - 10/03. Inventory Game/src/Main.java
+++ Ulangan 1 - 10/03. Inventory Game/src/Main.java
@@ -2,7 +2,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    class Items<I> {
+        private I nama;
+        private I jumlah;
 
+        Items(I nama, I jumlah) {
+            this.nama = nama;
+            this.jumlah = jumlah;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        HashMap<Items> items = new HashMap<>();
     }
 }
