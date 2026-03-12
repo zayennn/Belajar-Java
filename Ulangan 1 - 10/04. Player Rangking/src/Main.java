2a0eb1c50ceece3ef8ec8e2c7bb011f8a4e47fda
zayennn <zaayeennn@gmail.com>
2026-03-12 16:47:51 +0700

    update

--- Ulangan 1 - 10/04. Player Rangking/src/Main.java
+++ Ulangan 1 - 10/04. Player Rangking/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     static class Player {
@@ -14,11 +15,28 @@ public class Main {
             System.out.printf("""
                     Nama    : %s
                     Score   : %d
+                    
                     """, nama, score);
         }
     }
 
     public static void main(String[] args) {
         ArrayList<Player> players = new ArrayList<>();
+
+        players.addAll(Arrays.asList(
+            new Player("Player 1", 256),
+            new Player("Player 2", 451),
+            new Player("Player 3", 342),
+            new Player("Player 4", 225),
+            new Player("Player 5", 299)
+        ));
+
+        players.sort((a, b) -> b.score - a.score);
+
+        for (Player player : players) {
+            if (player.score >= 300) {
+                player.getInfo();
+            }
+        }
     }
 }
