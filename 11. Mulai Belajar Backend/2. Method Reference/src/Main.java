5773a6d031ebba16dfe2480a9841fac22e85e3e3
zayennn <zaayeennn@gmail.com>
2026-03-13 20:30:48 +0700

    update

--- 11. Mulai Belajar Backend/2. Method Reference/src/Main.java
+++ 11. Mulai Belajar Backend/2. Method Reference/src/Main.java
@@ -1,4 +1,6 @@
 // terkadang lambda itu masih terlalu panjang , contoh :
 // players.forEach(p -> p.getInfo());
 // java punya cara lain :
-//
+// players.forEach(Player::getInfo()); → ini disebut method reference
+// bentuk umumnya :
+// Class::method
