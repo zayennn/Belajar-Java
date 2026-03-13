3f6483f1af985e15e4eb584d6505c6d218a98205
zayennn <zaayeennn@gmail.com>
2026-03-13 20:22:35 +0700

    update

--- 11. Mulai Belajar Backend/1. Functional Interface/src/Main.java
+++ 11. Mulai Belajar Backend/1. Functional Interface/src/Main.java
@@ -6,8 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Hitung penjumlahan = ((x, y) -> x + y);
         Hitung perkalian = ((x, y) -> x * y);
+//        kalau gapake lambda code nya bisa panjang, contoh :
+
+        Hitung pengurangan = new Hitung() {
+            @Override
+            public int operasi(int x, int y) {
+                return x - y;
+            }
+        };
 
         System.out.println("hasil penjumlahan nya adalah : " + penjumlahan.operasi(10, 20));
         System.out.println("hasil perkalian nya adalah : " + perkalian.operasi(10, 20));
+        System.out.println("hasil pengurangan nya adalah : " + pengurangan.operasi(20, 10));
     }
 }
