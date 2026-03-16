453df41aa41db431e4e46b92be79c6caf3e073d4
zayennn <zaayeennn@gmail.com>
2026-03-16 16:06:30 +0700

    update

--- 11. Mulai Belajar Backend/1. Functional Interface/src/Main.java
+++ 11. Mulai Belajar Backend/1. Functional Interface/src/Main.java
@@ -16,10 +16,24 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Hitung tambah = (a, b) -> a + b;
-        Hitung perkalian = (a, b) -> a * b;
+//        Hitung tambah = (a, b) -> a + b;
+//        Hitung perkalian = (a, b) -> a * b;
 
-        System.out.println("hasil tambah = " +  tambah.operasi(10, 20));
-        System.out.println("hasil perkalian = " +  perkalian.operasi(10, 20));
+//      ada cara lain selain menggunakann lambda expression dengan cara membuat object baru , contoh nya :
+        Hitung tambah = new Hitung() {
+            @Override
+            public int operasi(int a, int b) {
+                return a + b;
+            }
+        };
+        Hitung perkalian = new Hitung() {
+            @Override
+            public int operasi(int a, int b) {
+                return a + b;
+            }
+        };
+
+        System.out.println("hasil tambah = " + tambah.operasi(10, 20));
+        System.out.println("hasil perkalian = " + perkalian.operasi(10, 20));
     }
 }
