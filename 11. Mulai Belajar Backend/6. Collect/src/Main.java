ab270b4f67c5e8833a3639f9227b01a0246dbeee
zayennn <zaayeennn@gmail.com>
2026-03-16 16:56:40 +0700

    update

--- 11. Mulai Belajar Backend/6. Collect/src/Main.java
+++ 11. Mulai Belajar Backend/6. Collect/src/Main.java
@@ -1,3 +1,20 @@
 // collect() digunakan untuk mengubah stream menjadi collection lagi
 // contoh : filter player → jadi list baru
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    static class Player {
+        String nama;
+        int score;
+
+        Player(String nama, int score) {
+            this.nama = nama;
+            this.score = score;
+        }
+    }
+
+
+}
