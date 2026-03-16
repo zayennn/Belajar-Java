0f37cf6e49f47bf9a5873e90f5651aa2bdbfe109
zayennn <zaayeennn@gmail.com>
2026-03-16 17:01:36 +0700

    update

--- 11. Mulai Belajar Backend/6. Collect/src/Main.java
+++ 11. Mulai Belajar Backend/6. Collect/src/Main.java
@@ -2,6 +2,7 @@
 // contoh : filter player → jadi list baru
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,5 +17,22 @@ public class Main {
         }
     }
 
+    public static void main(String[] args) {
+        ArrayList<Player> players = new ArrayList<>();
 
+        players.addAll(Arrays.asList(
+                new Player("player 1", 100),
+                new Player("player 2", 250),
+                new Player("player 3", 375),
+                new Player("player 4", 287),
+                new Player("player 5", 310)
+        ));
+
+        List<Player> high_score =
+                players.stream()
+                        .filter(player -> player.score > 250)
+                        .collect(Collectors.toList());
+
+        System.out.println("jumlah player high score : " + high_score.size());
+    }
 }
