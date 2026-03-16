243f58742c672994371dd3acbf3d01c0eb385329
zayennn <zaayeennn@gmail.com>
2026-03-16 16:50:50 +0700

    update

--- 11. Mulai Belajar Backend/5. Reduce/src/Main.java
+++ 11. Mulai Belajar Backend/5. Reduce/src/Main.java
@@ -29,3 +29,12 @@ public class Main {
         System.out.println(total_score);
     }
 }
+
+// .reduce(0, (a,b) -> a + b)
+// parameter pertama (0) adalah nilai awal
+// .reduce(0, (a,b) -> a + b) adalah lamnda , artinya : akumulasi data, prosesnya :
+/*
+    0 + 100
+    100 + 328
+    428 + 254
+*/
