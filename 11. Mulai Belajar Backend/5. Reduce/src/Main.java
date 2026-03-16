c4b595f24946bddc4a478a7b082d11818c07dfec
zayennn <zaayeennn@gmail.com>
2026-03-16 16:47:57 +0700

    update

--- 11. Mulai Belajar Backend/5. Reduce/src/Main.java
+++ 11. Mulai Belajar Backend/5. Reduce/src/Main.java
@@ -1,6 +1,7 @@
 // reduce() digunakan untuk menggabungkan semua data menjadi 1 nilai, contoh : mejumlahkan score
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     static class Player {
@@ -12,6 +13,19 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        ArrayList<Player> players = new ArrayList<>();
 
+        players.addAll(Arrays.asList(
+                new Player(100),
+                new Player(328),
+                new Player(254)
+        ));
+
+        int total_score =
+                players.stream()
+                        .map(player -> player.score)
+                        .reduce(0, (a, b) -> a + b);
+
+        System.out.println(total_score);
     }
 }
