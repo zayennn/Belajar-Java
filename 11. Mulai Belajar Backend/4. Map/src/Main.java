9840b0a02be4fa357e563c15ef8963c7ee148cea
zayennn <zaayeennn@gmail.com>
2026-03-16 16:26:17 +0700

    update

--- 11. Mulai Belajar Backend/4. Map/src/Main.java
+++ 11. Mulai Belajar Backend/4. Map/src/Main.java
@@ -4,3 +4,19 @@
 // contoh program :
 
 import java.util.ArrayList;
+
+public class Main{
+    static class Player {
+        private String nama;
+        private int score;
+
+        Player(String nama, int score) {
+            this.nama = nama;
+            this.score = score;
+        }
+
+        void getInfo() {
+            System.out.println("Nama : " + nama + " | " + "Score : " + score);
+        }
+    }
+}
