4d3f785d55d80b174a51af4a12340ad55e3c11f0
zayennn <zaayeennn@gmail.com>
2026-03-16 17:06:56 +0700

    update

--- 11. Mulai Belajar Backend/Latihan 11/1. Operati Matematika/src/Main.java
+++ 11. Mulai Belajar Backend/Latihan 11/1. Operati Matematika/src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         int hitung(int a, int b);
     }
 
-
+    public static void main(String[] args) {
+        OperatiMatematika penjumlahan = ((a, b) -> a + b);
+        OperatiMatematika pengurangan = ((a, b) -> a - b);
+        OperatiMatematika perkalian = ((a, b) -> a * b);
+    }
 }
