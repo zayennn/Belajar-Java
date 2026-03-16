b58fc974e50980f33281c036fa9d38e7a71a83de
zayennn <zaayeennn@gmail.com>
2026-03-16 15:45:30 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,90 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.text.NumberFormat;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static class Buku {
+        String judul;
+        String genre;
+        int harga;
+
+        private static final Locale indonesia = new Locale("id", "ID");
+        private static final NumberFormat formatRupiah;
+
+        static {
+            formatRupiah = NumberFormat.getCurrencyInstance(indonesia);
+            formatRupiah.setMaximumFractionDigits(0);
+            formatRupiah.setMinimumFractionDigits(0);
+        }
+
+        Buku(String judul, String genre, int harga) {
+            this.judul = judul;
+            this.genre = genre;
+            this.harga = harga;
+        }
+
+        void getInfoBukus() {
+            System.out.printf("""
+                    Judul buku  : %s
+                    Genre       : %s
+                    Harga       : %s
+                    
+                    """, judul, genre, formatRupiah.format(harga));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Buku> bukus = new ArrayList<>();
+
+        while(true) {
+            System.out.println("""
+                    ============================ Daftar Menu ============================
+                    1. Tambah Buku
+                    2. Lihat List Buku
+                    3. Keluar
+                    """);
+
+            System.out.print("Masukan pilihan anda (1/2/3) : ");
+            int menu = input.nextInt();
+            input.nextLine();
+
+            System.out.println("");
+
+            switch (menu) {
+                case 1 :
+                    System.out.print("Masukan judul buku : ");
+                    String judul = input.nextLine();
+                    System.out.print("Masukan genre buku : ");
+                    String genre = input.nextLine();
+                    System.out.print("Masukan harga buku : ");
+                    int harga = input.nextInt();
+
+                    bukus.add(new Buku(judul, genre, harga));
+
+                    System.out.println("Berhasil menambahkan buku " + judul);
+
+                    break;
+
+                case 2 :
+                    if (bukus.isEmpty()) {
+                        throw new IllegalArgumentException("Belum ada buku, tambah buku dulu");
+                    } else {
+                        for (Buku buku : bukus) {
+                            buku.getInfoBukus();
+                        }
+                    }
+
+                    break;
+
+                case 3 :
+                    System.out.print("Terimakasih...");
+                    return;
+
+                default:
+                    throw new IllegalArgumentException("Menu yang anda pilih tidak valid!");
+            }
         }
     }
 }
