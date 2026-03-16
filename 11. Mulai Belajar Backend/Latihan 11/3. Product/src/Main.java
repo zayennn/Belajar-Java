fe8725f36de9cb35122923fd5723e9865f79eb42
zayennn <zaayeennn@gmail.com>
2026-03-16 17:16:28 +0700

    update

--- 11. Mulai Belajar Backend/Latihan 11/3. Product/src/Main.java
+++ 11. Mulai Belajar Backend/Latihan 11/3. Product/src/Main.java
@@ -1,1 +1,15 @@
-import
+import java.util.ArrayList;
+
+public class Main {
+    static class Product {
+        String nama;
+        int harga;
+
+        Product(String nama, int harga) {
+            this.nama = nama;
+            this.harga = harga;
+        }
+    }
+
+
+}
