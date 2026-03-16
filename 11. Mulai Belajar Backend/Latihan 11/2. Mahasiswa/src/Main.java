0efaa528265fdab29c5f10d64b0762fcf86fe5cb
zayennn <zaayeennn@gmail.com>
2026-03-16 17:11:16 +0700

    update

--- 11. Mulai Belajar Backend/Latihan 11/2. Mahasiswa/src/Main.java
+++ 11. Mulai Belajar Backend/Latihan 11/2. Mahasiswa/src/Main.java
@@ -1,1 +1,19 @@
 // latihan 2
+
+import java.util.ArrayList;
+
+public class Main {
+    static class Mahasiswa {
+        String nama;
+        int nilai;
+
+        Mahasiswa(String nama, int nilai) {
+            this.nama = nama;
+            this.nilai = nilai;
+        }
+
+        void tampil() {
+            System.out.println("Nama : " + nama + " | " + "Nilai : " + nilai);
+        }
+    }
+}
