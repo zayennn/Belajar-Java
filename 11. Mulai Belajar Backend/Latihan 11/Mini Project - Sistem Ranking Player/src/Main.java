9e7c832683e3455998c86dd0dd19b378e2695e82
zayennn <zaayeennn@gmail.com>
2026-03-16 17:24:13 +0700

    update

--- 11. Mulai Belajar Backend/Latihan 11/Mini Project - Sistem Ranking Player/src/Main.java
+++ 11. Mulai Belajar Backend/Latihan 11/Mini Project - Sistem Ranking Player/src/Main.java
@@ -1,5 +1,7 @@
 // mini project
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     static class Player {
@@ -14,5 +16,19 @@ public class Main {
         void tampil() {
             System.out.println("Nama : " + nama + " | " + "Score : " + score);
         }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Player> players = new ArrayList<>();
+
+        players.addAll(Arrays.asList(
+                new Player("player 1", 250),
+                new Player("player 2", 226),
+                new Player("player 3", 375),
+                new Player("player 4", 410),
+                new Player("player 5", 300)
+        ));
+
+
     }
 }
