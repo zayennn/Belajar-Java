9ac0b4445b7be21ece3567d1407e35bda95224ae
zayennn <zaayeennn@gmail.com>
2026-03-16 16:12:39 +0700

    update

--- 11. Mulai Belajar Backend/2. Method Reference/src/Main.java
+++ 11. Mulai Belajar Backend/2. Method Reference/src/Main.java
@@ -7,3 +7,23 @@
 // ini di sebut dengan method reference
 // bentuk umum nya adalah : Class::method
 
+// contoh program :
+import java.util.ArrayList;
+
+public class Main {
+    static class Player {
+        private String nama;
+        private int score;
+
+        Player(String nama, int score) {
+            this.nama = nama;
+            this.score = score;
+        }
+
+        void getInfo() {
+            System.out.println("Nama : " + nama + " | " + "Score : " + score);
+        }
+    }
+
+
+}
