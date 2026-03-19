ab1a879aeefdbfa9b29c6055570b014854cc0a6e
zayennn <zaayeennn@gmail.com>
2026-03-19 12:12:46 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,17 @@
-public
+import java.util.HashMap;
+
+public class Main {
+    static class User<U> {
+        private U name;
+        private U age;
+
+        User(U name, U age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
