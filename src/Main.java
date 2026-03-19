ea74f5817b58bb89d4dd436bbf76731adea2b840
zayennn <zaayeennn@gmail.com>
2026-03-19 12:26:07 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 import java.util.ArrayList;
 
+public class Main {
+    static class User {
+        private String name;
+        private int age;
+
+        User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        void getInfo() {
+            System.out.printf("""
+                    Name    : %s
+                    Age     : %d
+                    """, name, age);
+        }
+    }
+}
