6e7d3c3b14a87834bb7349739840fa84df156469
zayennn <zaayeennn@gmail.com>
2026-03-23 12:13:50 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/Main.java
+++ Projects/1. Sistem Manajemen Karyawan/src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+import java.text.NumberFormat;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static class Karyawan implements Comparable<Karyawan> {
+        private String nama;
+        private int umur;
+        private int gaji;
+
+
+
+        @Override
+        public int compareTo(Karyawan o) {
+            return 0;
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
