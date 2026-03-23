4aa76bb50d34f27b395c8adba3d47a336a35197d
zayennn <zaayeennn@gmail.com>
2026-03-23 12:59:10 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/service/KaryawanService.java
+++ Projects/1. Sistem Manajemen Karyawan/src/service/KaryawanService.java
@@ -2,14 +2,25 @@ package service;
 
 import model.Karyawan;
 import java.util.Locale;
-import
+import java.text.NumberFormat;
 
 public class KaryawanService {
+    static class FormatNumber {
+        private final static  Locale indonesia = new Locale("id", "ID");
+        private final static NumberFormat rupiah;
+
+        static {
+            rupiah = NumberFormat.getCurrencyInstance(indonesia);
+            rupiah.setMaximumFractionDigits(0);
+            rupiah.setMinimumFractionDigits(0);
+        }
+    }
+
     public static void getInfoKaryawan(Karyawan karyawan) {
         System.out.printf("""
                 Nama    : %s
                 Umur    : %d
-                Gaji    : %d
-                """, karyawan.nama, karyawan.umur, karyawan.gaji);
+                Gaji    : %s
+                """, karyawan.nama, karyawan.umur, FormatNumber.rupiah.format(karyawan.gaji));
     }
 }
