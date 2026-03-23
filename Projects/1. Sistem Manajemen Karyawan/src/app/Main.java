08f3301038795ed9f6cc89ce9e156b9c824fc258
zayennn <zaayeennn@gmail.com>
2026-03-23 13:06:58 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
+++ Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
@@ -7,7 +7,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Karyawan karyawan = new Karyawan("karyawan 1", 20, 1000000);
-        KaryawanService.getInfoKaryawan(karyawan);
+        ArrayList<Karyawan> karyawans = new ArrayList<>();
+
+        karyawans.addAll(java.util.Arrays.asList(
+                new Karyawan("karyawan 1", 20, 1000000),
+                new Karyawan("karyawan 2", 19, 3000000),
+                new Karyawan("karyawan 3", 30, 2000000)
+        ));
     }
 }
--- Projects/1. Sistem Manajemen Karyawan/src/model/Karyawan.java
+++ Projects/1. Sistem Manajemen Karyawan/src/model/Karyawan.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Karyawan {
+import java.util.Collections;
+
+public class Karyawan implements Comparable<Karyawan> {
     public String nama;
     public int umur;
     public int gaji;
@@ -9,5 +11,10 @@ public class Karyawan {
         this.nama = nama;
         this.umur = umur;
         this.gaji = gaji;
+    }
+
+    @Override
+    public int compareTo(Karyawan other) {
+        return other.gaji - this.gaji;
     }
 }
