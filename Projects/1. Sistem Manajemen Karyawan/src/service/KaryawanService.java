5d44cadd755f2765f9fae67d2e95fb6567439a55
zayennn <zaayeennn@gmail.com>
2026-03-27 14:00:10 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/service/KaryawanService.java
+++ Projects/1. Sistem Manajemen Karyawan/src/service/KaryawanService.java
@@ -2,6 +2,9 @@ package service;
 
 import model.Karyawan;
 
+import java.util.Locale;
+import java.text.NumberFormat;
+
 public class KaryawanService {
     public void getInfoKaryawan(Karyawan k) {
         System.out.printf("""
