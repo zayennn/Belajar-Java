d29005237307b8793c3933cfb22556b2f49f8701
zayennn <zaayeennn@gmail.com>
2026-03-27 13:00:09 +0700

    update

---
+++ Projects/1. Sistem Manajemen Karyawan/src/model/Karyawan.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Karyawan {
+    String nama;
+    int umur;
+    int gaji;
+
+    Karyawan(String nama, int umur, int gaji) {
+        this.nama = nama;
+        this.umur = umur;
+        this.gaji = gaji;
+    }
+}
