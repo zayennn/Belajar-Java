0d59ba09b8c6605cb85287799fb42654c6251ebd
zayennn <zaayeennn@gmail.com>
2026-03-27 14:05:59 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
+++ Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
@@ -4,21 +4,19 @@ import model.Karyawan;
 import service.KaryawanService;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Karyawan> karyawans = new ArrayList<>();
         KaryawanService service = new KaryawanService();
 
-        karyawans.addAll(java.util.Arrays.asList(
-                new Karyawan("elang", 19, 1000000),
-
-                new Karyawan("zayen", 19, 1000000),
-                new Karyawan("alan", 19, 1000000)
-        ));
-
-        Karyawan.perusahaan = "google";
-
-        karyawans.forEach(service::getInfoKaryawan);
+        while(true) {
+            System.out.println("""
+                    ================== Sistem Manajemen Karyawan ==================
+                    1. Tambah Karyawan
+                    2. 
+                    """);
+        }
     }
 }
