fa49d830e9e19fe3416177c0bc04b20fe75173e8
zayennn <zaayeennn@gmail.com>
2026-03-30 07:55:13 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/service/EmployeeService.java
+++ Projects/1. Sistem Manajemen Karyawan/src/service/EmployeeService.java
@@ -3,8 +3,11 @@ package service;
 import model.Employee;
 
 public class EmployeeService {
-    public void getInfoEmplooyes() {
+    void getInfoEmplooyes(Employee emp) {
         System.out.printf("""
-                """);
+                Name    : %s
+                Age     : %d
+                Salary  : %d
+                """, emp.name, emp.age, emp.salary);
     }
 }
