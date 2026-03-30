53e3b39fdc979746ed79ec88f923ebc4e783bd4b
zayennn <zaayeennn@gmail.com>
2026-03-30 07:48:08 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
+++ Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
@@ -3,11 +3,17 @@ package app;
 import model.Employee;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Employee> employees = new ArrayList<>();
 
-
+        while (true) {
+            System.out.print("""
+                    ===================== Sistem Manajemen Karyawan =====================
+                    
+                    """);
+        }
     }
 }
