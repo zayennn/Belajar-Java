3d94b80baf6f61c4caf5c21ceb354ea740106fd9
zayennn <zaayeennn@gmail.com>
2026-03-30 07:46:41 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
+++ Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
@@ -8,14 +8,6 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Employee> employees = new ArrayList<>();
 
-        employees.addAll(java.util.Arrays.asList(
-                new Employee("karyawan 1", 20, 5000000),
-                new Employee("karyawan 2", 20, 3000000),
-                new Employee("karyawan 3", 20, 5400000)
-        ));
 
-        for (int i = 0; i < employees.toArray().length; i++) {
-            System.out.println(employees.get(i));
-        }
     }
 }
