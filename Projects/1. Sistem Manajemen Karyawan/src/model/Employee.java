3d069ef5eec1cb102dbe783c06cecd0df8d81f47
zayennn <zaayeennn@gmail.com>
2026-03-30 07:27:26 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/model/Employee.java
+++ Projects/1. Sistem Manajemen Karyawan/src/model/Employee.java
@@ -4,4 +4,10 @@ public class Employee {
     public String name;
     public int age;
     public int salary;
+
+    Employee(String name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary= salary;
+    }
 }
