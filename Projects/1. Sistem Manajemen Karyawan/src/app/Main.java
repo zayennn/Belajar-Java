07f52b62f4290ad4bd13389305211fad4f6a3c53
zayennn <zaayeennn@gmail.com>
2026-03-30 07:37:47 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
+++ Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
@@ -4,7 +4,14 @@ import model.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new Employee("karyawan 1", 20, 5000000);
-        System.out.println(employee);
+//        Employee employee = new Employee("karyawan 1", 20, 5000000);
+        Employee employee = new Employee();
+
+        employee.name = "karyawan 1";
+        employee.age = 20;
+        employee.salary = 5000000;
+
+
+        System.out.println(employee.name);
     }
 }
--- Projects/1. Sistem Manajemen Karyawan/src/model/Employee.java
+++ Projects/1. Sistem Manajemen Karyawan/src/model/Employee.java
@@ -1,13 +1,13 @@
 package model;
 
 public class Employee {
-    String name;
-    int age;
-    int salary;
+    public String name;
+    public int age;
+    public int salary;
 
-    public Employee(String name, int age, int salary) {
-        this.name = name;
-        this.age = age;
-        this.salary= salary;
-    }
+//    public Employee(String name, int age, int salary) {
+//        this.name = name;
+//        this.age = age;
+//        this.salary= salary;
+//    }
 }
