91d3566faf8e64e6df37e644eec747e4ab20a1e2
zayennn <zaayeennn@gmail.com>
2026-03-31 11:40:38 +0700

    update

--- Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
+++ Projects/1. Sistem Manajemen Karyawan/src/app/Main.java
@@ -59,7 +59,7 @@ public class Main {
                     System.out.print("Siapa nama karyawan yang ingin anda hapus ? : ");
                     String name = input.nextLine();
 
-                    employees.removeIf(employee -> employee.equals(String.format("%s", name)));
+                    employees.removeIf(employee -> employee.equals(name));
 
                     break;
 
